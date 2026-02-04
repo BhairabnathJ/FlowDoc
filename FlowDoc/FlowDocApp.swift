23e30b5b62bdcc0bf7151248dda5413181757b05
BhairabnathJ <nepal.alex6@gmail.com>
2026-02-04 12:41:59 -0500

    Initial Commit

---
+++ FlowDoc/FlowDocApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowDocApp.swift
+//  FlowDoc
+//
+//  Created by Bhairabnath Jamkatel on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct FlowDocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
